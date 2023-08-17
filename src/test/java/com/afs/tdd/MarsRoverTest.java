4b6dcfd4e0e4b73e0ffae9e269f575b16880b35a
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-17 20:05:48 +0800

    test: added test case for command TurnRight given location 0,0,E

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -153,4 +153,19 @@ class MarsRoverTest {
         Assertions.assertEquals(0, currentLocation.getY());
         Assertions.assertEquals(Direction.WEST, currentLocation.getDirection());
     }
+
+    @Test
+    void should_change_to_location_0_0_S_when_executeCommand_given_0_0_E_and_command_TurnRight(){
+        //Given
+        Location initialLocation = new Location(0, 0, Direction.EAST);
+        Command givenCommand = Command.TURN_RIGHT;
+        MarsRover marsRover = new MarsRover(initialLocation);
+        //When
+        marsRover.executeCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+        //Then
+        Assertions.assertEquals(0, currentLocation.getX());
+        Assertions.assertEquals(0, currentLocation.getY());
+        Assertions.assertEquals(Direction.SOUTH, currentLocation.getDirection());
+    }
 }
